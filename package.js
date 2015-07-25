a2ed0a1e2a942275d0fd9209e05a1af9f43715c7
jorjordandan <jordan@jordandavis.cs>
2015-07-24 22:40:13 -0700

    change name of package back

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "jorjordandan:fabricjs",
+  name: "ryanswapp:fabricjs",
   summary: "Wrapper for Fabric.js, a javascript library for working with canvas",
   version: "0.1.1",
   git: "https://github.com/ryanswapp/fabricjs"
